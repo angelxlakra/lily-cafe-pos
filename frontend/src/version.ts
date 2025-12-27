e4fe798176d1361d757243846a35f77a0b3fc44c
Angel Lakra <angel.lakra@chainlabs.in>
2025-12-27 14:04:49 +0530

    chore: bump version to 0.1.2

--- frontend/src/version.ts
+++ frontend/src/version.ts
@@ -2,15 +2,15 @@
  * Version information for Lily Cafe POS System Frontend
  */
 
-export const VERSION = '0.1.0';
+export const VERSION = '0.1.2';
 
 export const VERSION_INFO = {
   version: VERSION,
   major: 0,
   minor: 1,
-  patch: 0,
-  releaseDate: '2025-11-11',
-  releaseName: 'Initial MVP',
+  patch: 2,
+  releaseDate: '2025-12-27',
+  releaseName: 'Partial Serving & Payment Editing',
 };
 
 export function getVersion(): string {
