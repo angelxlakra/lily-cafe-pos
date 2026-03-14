ad0abf501887aec0a4d9ed033d1b133fcc89d5b7
Angel Lakra <angel.lakra@chainlabs.in>
2026-03-14 13:19:41 +0530

    feat(design): add LinenTexture to AdminLayout and switch to off-white background

--- frontend/src/components/AdminLayout.tsx
+++ frontend/src/components/AdminLayout.tsx
@@ -1,16 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
+import LinenTexture from "./LinenTexture";
 import { useSidebar } from "../context/SidebarContext";
 
 export default function AdminLayout() {
   const { isCollapsed } = useSidebar();
 
   return (
-    <div className="flex min-h-screen bg-neutral-background">
+    <div className="flex min-h-screen bg-off-white relative">
+      <LinenTexture />
       <Sidebar />
       <main
         className={`
-          flex-1 transition-all duration-300 ease-in-out
+          flex-1 transition-all duration-300 ease-in-out relative z-10
           ${isCollapsed ? "lg:ml-20" : "lg:ml-60"}
         `}
       >
